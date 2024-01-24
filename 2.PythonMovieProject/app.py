a26e69102b3982b56b486f49107211674a613e65
Nicolas <leali.nicolas@hotmail.com>
2024-01-24 19:00:21 -0300

    se agregan try Except

--- 2.PythonMovieProject/app.py
+++ 2.PythonMovieProject/app.py
@@ -1,4 +1,32 @@
 from datetime import datetime
+import database
+
+def prompt_add_movie():
+    try:
+        title = input('Movie title: ')
+        release_date = input('Release date (dd-mm-yyyy): ')
+        parsed_date = datetime.strptime(release_date,"%d-%m-%Y")
+        timestamp = parsed_date.timestamp()
+        error = database.add_movie(title, timestamp)
+        print(error)
+    except Exception as e:
+        print(e)
+
+
+def print_upcoming_movies(tipo, movies):
+    print(f"-----{tipo} MOVIES----")
+    for movie in movies:
+        movie_date = datetime.fromtimestamp(movie[1])
+        human_date = movie_date.strftime("%b %d %Y")
+        print(f"Pelicula = { movie[0]} fecha = {human_date}")
+    print("\n\n")
+
+def prompt_watched_movie():
+    try:
+        user_input_title = input('Ingrese la pelicula: ')
+        database.watch_movie(user_input_title)
+    except Exception as e:
+        print(e)
 
 menu = """Please select one of the following options:
 1) Add new movie.
@@ -11,6 +39,7 @@ menu = """Please select one of the following options:
 Your Selection: """
 
 welcome = 'Bienvenido al sistema de peliculas'
+database.create_tables()
 
 print(welcome)
 
@@ -19,13 +48,13 @@ user_input = input(menu)
 while user_input != '6':
 
     if user_input == '1':
-        pass
+        prompt_add_movie()
     elif user_input == '2':
-        pass
+        print_upcoming_movies('Upcoming', database.get_movies(True))
     elif user_input == '3':
-        pass
+        print_upcoming_movies('All', database.get_movies(False))
     elif user_input == '4':
-        pass
+        prompt_watched_movie()
     elif user_input == '5':
         pass
     else:
--- 2.PythonMovieProject/database.py
+++ 2.PythonMovieProject/database.py
@@ -8,10 +8,11 @@ release_timestamp REAL,
 watched INTEGER);
 """
 
-INSERT_MOVIES = "INSERT INTO movies (title, release_timestamp, watched) VALUES (?, ?, 0);"
+INSERT_MOVIES = "INSERT INTO movie (title, release_timestamp, watched) VALUES (?, ?, 0);"
 SELECT_ALL_MOVIES = "SELECT * FROM movies;"
 SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
 SELECT_WATCHED_MOVIES = "SELECT * FROM movies WHERE watched = 1;"
+SET_MOVIE_WATCHES = "UPDATE movies SET watched = 1 where title = ?;"
 
 connection = sqlite3.connect('..//sqlite//bd_movies//bd_movie.db')
 
@@ -21,14 +22,17 @@ def create_tables():
         connection.execute(CREATE_MOVIES_TABLE)
 
 def add_movie(title, release_timestamp):
-    with connection:
-        connection.execute(INSERT_MOVIES, (title, release_timestamp))
+    try:
+        with connection:
+            connection.execute(INSERT_MOVIES, (title, release_timestamp))
+    except Exception as e:
+        return ('Error de insert' + str(e))
 
 def get_movies(upcoming=False):
     with connection:
         cursor = connection.cursor()
         if upcoming:
-            today_timestamp = datetime.datetime.today().today_timestamp()
+            today_timestamp = datetime.datetime.today().timestamp()
             cursor.execute(SELECT_UPCOMING_MOVIES, (today_timestamp,))
         else:
             cursor.execute(SELECT_ALL_MOVIES)
@@ -36,7 +40,11 @@ def get_movies(upcoming=False):
         return cursor.fetchall()
 
 def watch_movie(title):
-    pass
+    try:
+        with connection:
+            connection.execute(SET_MOVIE_WATCHES, (title,))
+    except Exception as e:
+        print('La pelicula no existe')
 
 def get_movie_watch():
     with connection:
