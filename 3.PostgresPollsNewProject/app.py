3d77c575618053e52f188c4d9d2630d6da56b645
Nicolas <leali.nicolas@hotmail.com>
2024-01-30 20:37:09 -0300

    Crea conexion y excepciones

--- 3.PostgresPollsNewProject/app.py
+++ 3.PostgresPollsNewProject/app.py
@@ -3,29 +3,17 @@ from dotenv import load_dotenv
 import os
 import psycopg2
 
-
-
 try:
     load_dotenv()
     connection = psycopg2.connect(database=os.getenv('POSTGRES_DB'), 
                               user=os.getenv('POSTGRES_USER'), 
-                              password=os.getenv('POSTGRES_PASSWORD'))
-except Exception as error:
+                              password=os.getenv('POSTGRES_PASSWORD'),
+                              host='localhost',
+                              port=5432)
+except (Exception, psycopg2.DatabaseError) as error:
     print(f"Error en crea conexion {error}")
 
-
-def crea_tablas():
-    try:
-        with connection:
-            with connection.cursor() as cursor:
-                cursor.execute(database.CREATE_OPTIONS)
-                cursor.execute(database.CREATE_POLLS)
-                cursor.execute(database.CREATE_VOTES)
-    except Exception as error:
-        return f"Error en crea tablas " + str(error)
-    
-
 try:
-    crea_tablas()
+    database.crea_tablas(psycopg2, connection)
 except Exception as error:
     print(error)
--- 3.PostgresPollsNewProject/database.py
+++ 3.PostgresPollsNewProject/database.py
@@ -1,9 +1,38 @@
 CREATE_POLLS = """CREATE TABLE IF NOT EXISTS polls 
-(id SERIAL PRIMARY KEY, title TEXT, owner_username TEXT);
-"""
-
+(id SERIAL PRIMARY KEY, title TEXT, owner_username TEXT);"""
 CREATE_OPTIONS = """CREATE TABLE IF NOT EXISTS options(
 id SERIAL PRIMARY KEY, option_text TEXT, poll_id INTEGER, FOREIGN KEY(poll_id) REFERENCES polls(id));"""
-
 CREATE_VOTES = """CREATE TABLE IF NOT EXISTS votes
-(username TEXT, option_id INTEGR FOREIGN KEY(option_id) REFERENCES options(id));"""
+(username TEXT, option_id INTEGER, FOREIGN KEY(option_id) REFERENCES options(id));"""
+
+SELECT_ALL_POLLS = """SELECT * FROM polls; """
+SELECT_POLL_WITH_OPTIONS = """SELECT poll.id, options.option_text 
+FROM polls 
+INNER JOIN options 
+ON polls.id = options.poll_id
+WHERE polls.id = %s;"""
+
+
+
+
+
+
+def crea_tablas(psycopg2, connection):
+    try:
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(CREATE_POLLS)
+                cursor.execute(CREATE_OPTIONS)
+                cursor.execute(CREATE_VOTES)
+    except (Exception, psycopg2.DataError) as error:
+        raise  error  
+    
+
+def get_polls(connection):
+    try:
+        with connection:
+            with connection.cursor as cursor:
+                cursor.execute(SELECT_ALL_POLLS)
+                return cursor.fetchall()
+    except (Exception, psycopg2.DatabaseError) as error:
+        raise error
